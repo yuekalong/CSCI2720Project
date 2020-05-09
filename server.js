08fc44d3fac62bcd6e36f1726d93fbc4595ed080
MangChun07 <49334289+MangChun07@users.noreply.github.com>
2020-05-09 23:24:52 +0800

    add yelp-fusion package

--- server.js
+++ server.js
@@ -3,18 +3,11 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const locations = require('./routes/api/locations');
-const users = require('./routes/api/users');
-const flists = require('./routes/api/favoriteLists');
-const admins = require('./routes/api/admins');
-const lclists = require('./routes/api/locationCommentLists');
-const comments = require('./routes/api/comments');
-
 const app = express();
 
 app.use(bodyParser.json());
 
-var dbUri = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false';
+var dbUri = 'mongodb+srv://JackyChun:qwer1234@cluster0-wt6nl.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true';
 mongoose.connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 mongoose.set('useCreateIndex', true);
@@ -22,8 +15,15 @@ mongoose.set('useCreateIndex', true);
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function () {
-	console.log("Connection is open...");
+	console.log("Connection is opened...");
 });
+
+const locations = require('./routes/api/locations');
+const users = require('./routes/api/users');
+const flists = require('./routes/api/favoriteLists');
+const admins = require('./routes/api/admins');
+const lclists = require('./routes/api/locationCommentLists');
+const comments = require('./routes/api/comments');
 
 app.use('/api/locations', locations);
 app.use('/api/users', users);
@@ -32,25 +32,6 @@ app.use('/api/admins', admins);
 app.use('/api/locationCommentLists', lclists);
 app.use('/api/comments', comments);
 
-//const db = require();
-/*
-mongoose
-    .connetn(db)
-    .then(() => console.log('db connented'))
-    .catch(err => console.log(err));
-*/
-//use route
-//app.use('/api/items', items);
-/*
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-*/
 app.all('*', function(req, res) {
     res.send('hello world');
   });
