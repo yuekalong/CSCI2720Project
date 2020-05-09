0cc7ee3f410d5709d0d97e1dd65d93adbab2d6cd
jacky310 <jackyng310@gmail.com>
2020-05-10 01:38:02 +0800

    change something on index.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use('/api/admins', admins);
 app.use('/api/locationCommentLists', lclists);
 app.use('/api/comments', comments);
 
-app.get('*', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
