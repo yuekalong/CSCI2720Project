8a686070925c15e28e54f24c4a3387eeedde291d
jacky310 <jackyng310@gmail.com>
2020-05-11 00:03:48 +0800

    make some about the router and model

--- model/user.model.js
+++ model/user.model.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-const conn = mongoose.createConnection("mongodb+srv://jackyNg:jackyng@cluster0-7hx7m.gcp.mongodb.net/test?retryWrites=true&w=majority");
 
 var UserSchema = new Schema({
     userID: {type: Number, required: true, unique: true},
@@ -8,5 +7,4 @@ var UserSchema = new Schema({
     password: {type: String, required: true}
 });
 
-const User = conn.model('User', UserSchema);
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
